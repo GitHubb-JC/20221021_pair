5ad01151e924434d5c3d2513489b56fa4dc46922
JC <chohy1106@gmail.com>
2022-10-21 11:40:24 +0900

    review/index, create

---
+++ reviews/forms.py
@@ -0,0 +1,14 @@
+from socket import fromshare
+from django import forms
+from .models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        field = [
+            "movie_name",
+            "title",
+            "content",
+            "grade",
+        ]
